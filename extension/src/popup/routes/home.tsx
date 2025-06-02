21ce7b30c004774b83edc844dff98d664c48a105
Rohith Gilla <gillarohith1@gmail.com>
2025-06-02 19:20:42 +0530

    feat: basic extension

---
+++ extension/src/popup/routes/home.tsx
@@ -0,0 +1,10 @@
+import { CountButton } from "~features/count-button"
+
+export const Home = () => {
+  return (
+    <>
+      <h1>Clerk + Chrome Extension + React Router</h1>
+      <CountButton />
+    </>
+  )
+}
