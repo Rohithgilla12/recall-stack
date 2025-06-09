4151ea8cccb168caff09a1dc5761345dcf416c44
Rohith Gilla <gillarohith1@gmail.com>
2025-06-09 18:38:07 +0530

    chore:

---
+++ extension/src/example-convex-usage.ts
@@ -0,0 +1,8 @@
+// Example of how to use the shared Convex API in the extension
+import { api } from "../convex/_generated/api"
+
+// You can now use the api object to call your Convex functions
+// For example:
+// const products = await convexClient.query(api.products.get);
+
+export { api }
--- web/src/routes/demo.convex.tsx
+++ web/src/routes/demo.convex.tsx
@@ -1,34 +1,34 @@
-import { createFileRoute } from "@tanstack/react-router"
-import { useQuery } from "convex/react"
-import { Suspense } from "react"
+import { createFileRoute } from "@tanstack/react-router";
+import { useQuery } from "convex/react";
+import { Suspense } from "react";
 
-import { api } from "../../convex/_generated/api"
+import { api } from "../../../convex/_generated/api";
 
 export const Route = createFileRoute("/demo/convex")({
-	component: App,
-})
+  component: App,
+});
 
 function Products() {
-	const products = useQuery(api.products.get)
+  const products = useQuery(api.products.get);
 
-	return (
-		<ul>
-			{(products || []).map((p) => (
-				<li key={p._id}>
-					{p.clerkUserId} - {p.name} - {p.email}
-				</li>
-			))}
-		</ul>
-	)
+  return (
+    <ul>
+      {(products || []).map((p) => (
+        <li key={p._id}>
+          {p.clerkUserId} - {p.name} - {p.email}
+        </li>
+      ))}
+    </ul>
+  );
 }
 
 function App() {
-	return (
-		<div className="p-4">
-			<Suspense fallback={<div>Loading...</div>}>
-				<h1>Products</h1>
-				<Products />
-			</Suspense>
-		</div>
-	)
+  return (
+    <div className="p-4">
+      <Suspense fallback={<div>Loading...</div>}>
+        <h1>Products</h1>
+        <Products />
+      </Suspense>
+    </div>
+  );
 }
